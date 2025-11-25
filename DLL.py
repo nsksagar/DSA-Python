b06e73476a4a3691d2e00f8d19477fc63dc40744
sagar Nookarapu <sagarnookarapu@s172-20-38-h191.paws.uga.edu>
2025-11-25 13:16:48 -0500

    added new functionalities

--- DLL.py
+++ DLL.py
@@ -130,6 +130,87 @@ def remove_giv_node(temp):
     temp.prev = None 
 
 
+def insert_before_head(head, val):
+
+    new_node = Node(val)
+
+    if not head:
+        return new_node
+    
+    temp = head 
+
+    new_node.next = temp
+    temp.prev = new_node
+    head = new_node
+
+    return head
+
+def insert_before_tail(head, val):
+
+    new_node = Node(val)
+
+    if not head:
+        return new_node 
+
+    if head.next is None:
+        return insert_before_head(head, val)
+    
+    tail = head
+    
+    while tail.next:
+        tail = tail.next 
+
+    new_node.prev = tail.prev
+    tail.prev.next = new_node
+    tail.prev = new_node
+    new_node.next = tail
+
+
+    return head
+
+
+def insert_before_kth_node(head, val, k):
+
+    if k == 1: 
+        return insert_before_head(head, val)
+
+    # if not head:
+    #     return new_node 
+    
+    node = head
+    
+    count = 0 
+    while node is not None: 
+        count += 1
+        if count == k:
+            break
+
+        node = node.next 
+
+    new_node = Node(val)
+    new_node.prev = node.prev
+    new_node.next = node
+
+    node.prev.next = new_node 
+    node.prev = new_node
+    
+
+    return head
+
+def insert_given_node(temp, val):
+    back = temp.prev 
+    
+
+    new_node = Node(val) 
+
+    new_node.next = temp
+    new_node.prev = back
+
+    back.next = new_node
+    temp.prev = new_node
+
+
+
 if __name__ == '__main__':
 
     array = [10,20,30,40,50]
@@ -147,9 +228,21 @@ if __name__ == '__main__':
     #rem_kth = del_kth_element(dll_head, 3)
     #print_dll(rem_kth)
 
-    del_node = remove_giv_node(dll_head.next.next)
+    #del_node = remove_giv_node(dll_head.next.next)
+    #print_dll(dll_head)
+
+    insert_node = insert_given_node(dll_head.next, 1524)
     print_dll(dll_head)
 
+    #insert_head = insert_before_head(dll_head, 100)
+    #print_dll(insert_head)
+
+    # insert_kth_node = insert_before_kth_node(dll_head, 100, 3)
+    # print_dll(insert_kth_node)
+
+    
+    # insert_tail = insert_before_tail(dll_head, 100)
+    # print_dll(dll_head)
     #remove_head = del_head_DLL(dll_head)
     #print_dll(remove_head)
 
